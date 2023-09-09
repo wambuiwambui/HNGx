52255ecb0163c44bbab8ae229ca3954e67fe249a
wambuiwambui <tnjoki11@gmail.com>
2023-09-09 06:24:30 +0300

    done1

--- app.py
+++ app.py
@@ -5,29 +5,31 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def get_info():
-    # Get query parameters
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
-
-    # Get the current day of the week
-    current_day = datetime.datetime.now().strftime('%A')
-
-    # Get the current UTC time
+    
+    # Generate the current UTC time with the correct format
     utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-
-    # Construct JSON response
+    
+    # Your GitHub repository URL and file URL
+    github_repo_url = 'https://github.com/wambuiwambui/HNGx'
+    github_file_url = 'https://github.com/wambuiwambui/HNGx/blob/master/app.py'
+    
+    # Create the JSON response
     response = {
         "slack_name": slack_name,
-        "current_day": current_day,
+        "current_day": datetime.datetime.now().strftime('%A'),
         "utc_time": utc_time,
         "track": track,
-        "github_file_url": "https://github.com/wambuiwambui/HNGx/blob/master/app.py",
-        "github_repo_url": "https://github.com/wambuiwambui/HNGx",
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "password": "Bobo",  
         "status_code": 200
     }
-
+    
     return jsonify(response)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    
+from waitress import serve
+serve(app, host="0.0.0.0", port=5000)
+
+
