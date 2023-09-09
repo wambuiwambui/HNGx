f58d44cd7bbca1be22c6a9678ae3b2fe1edb152c
wambuiwambui <tnjoki11@gmail.com>
2023-09-09 06:41:07 +0300

    done1

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def get_info():
     
     return jsonify(response)
 
-from waitress import serve
-serve(app, host="0.0.0.0", port=5000)
-
+if __name__ == '__main__':
+    
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
